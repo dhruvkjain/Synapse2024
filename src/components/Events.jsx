00e750beabfef3d52cfeb41a0e96cb56ec29c4ba
Shah Nirmal Dipakkumar <nirmalshah20519@gmail.com>
2024-01-16 15:23:04 +0530

    started event page

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,13 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./Functional Components/Navbar";
+import EventCategoryCard from "./Functional Components/EventCategoryCard";
+import './Events.css'
 
 function Events({changePageinApp}){
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch('/Synapse-2024/Categories.json');
+            const jsonData = await response.json();
+            setData(jsonData);
+          } catch (error) {
+            console.error('Error fetching data:', error);
+          }
+        };
+    
+        fetchData();
+      }, []);
+
+      function handleExploreClick(){
+        console.log("Gello");
+      }
 
     return(
-        <div>
-           Events 
+        <>
            <Navbar changePageinApp={changePageinApp} />
-        </div>
+
+           <div className="event-page">
+            <div className="event-container">
+                <div className="event-heading">
+                    <span className="explore">Explore</span> Events
+                </div>
+            </div>
+            <div className="cards-container">
+                {
+                    data.map((d)=>
+                        <EventCategoryCard
+                            key={d.id}
+                            id={d.id}
+                            imageUrl={d.url}
+                            title={d.name}
+                            onClickExplore={handleExploreClick}
+                        />
+                    )
+                }
+            
+            </div>
+           </div>
+        </>
     )
 }
 
