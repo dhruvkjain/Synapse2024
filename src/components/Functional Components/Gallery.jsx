d0a51178ce9b83bb38643ea54c63ab4d2ca51f9f
Shah Nirmal Dipakkumar <nirmalshah20519@gmail.com>
2024-01-29 15:06:31 +0530

    Photos changes

--- src/components/Functional Components/Gallery.jsx
+++ src/components/Functional Components/Gallery.jsx
@@ -7,22 +7,22 @@ function Gallery() {
     const row1 = [
         {
             id:1,
-            src:'https://res.cloudinary.com/dzeuvqdbl/image/upload/v1705775205/synapse/photos%20sort%20drive/assem%20sharma/ATM_0487_mnbncv.jpg',
+            src:'src/assets/synapse/assem sharma/ATM_0452_qh5qqp.jpg',
             name:'Aseem Sharma'
         },
         {
             id:2,
-            src:'https://res.cloudinary.com/dzeuvqdbl/image/upload/v1705772186/synapse/photos%20sort%20drive/assem%20sharma/ATM_0452_qh5qqp.jpg',
+            src:'src/assets/synapse/Battle of Bands/IMG_0163_eiivgh.jpg',
             name:'Battle of Bands'
         },
         {
             id:3,
-            src:'https://res.cloudinary.com/dzeuvqdbl/image/upload/v1705772150/synapse/photos%20sort%20drive/dj%20war/IMG_7161_bnysmp.jpg',
+            src:'src/assets/synapse/dj_war/PXL_20230316_202349387_t7uwxt.jpg',
             name:'DJ War'
         },
         {
             id:4,
-            src:'https://res.cloudinary.com/dzeuvqdbl/image/upload/v1705772194/synapse/photos%20sort%20drive/innaugral/PXL_20230316_140116679_qrlyki.jpg',
+            src:'src/assets/synapse/innaugral/IMG_6951_umbjgp.jpg',
             name:'Inaugural'
         }
     ]
@@ -30,22 +30,22 @@ function Gallery() {
     const row2 = [
         {
             id:1,
-            src:'https://res.cloudinary.com/dzeuvqdbl/image/upload/v1705772253/synapse/photos%20sort%20drive/jonita%20gandhi/DSC03415_fpxnxq.jpg',
+            src:'src/assets/synapse/jonitagandhi/DSC03415_fpxnxq.jpg',
             name:'Jonita Gandhi'
         },
         {
             id:2,
-            src:'https://res.cloudinary.com/dzeuvqdbl/image/upload/v1705773708/synapse/photos%20sort%20drive/mime%20out%20%20loud/ATM_0166_kkceeh.jpg',
+            src:'src/assets/synapse/mimeoutloud/ATM_0162_r0mbtu.jpg',
             name:'Mime Out Loud'
         },
         {
             id:3,
-            src:'https://res.cloudinary.com/dzeuvqdbl/image/upload/v1705772145/synapse/photos%20sort%20drive/ktm/IMG_9296_i695nv.jpg',
+            src:'src/assets/synapse/ktm/IMG_9296_i695nv.jpg',
             name:'KTM'
         },
         {
             id:4,
-            src:'https://res.cloudinary.com/dzeuvqdbl/image/upload/v1705772157/synapse/photos%20sort%20drive/naach/PXL_20230319_133848843_p2knfv.jpg',
+            src:'src/assets/synapse/naach/PXL_20230319_133848843_p2knfv.jpg',
             name:'Naach'
         }
     ]
@@ -53,22 +53,22 @@ function Gallery() {
     const row3 = [
         {
             id: 1,
-            src: 'https://res.cloudinary.com/dzeuvqdbl/image/upload/v1705772256/synapse/photos%20sort%20drive/open%20mic/ATM_0389_le8n13.jpg',
+            src: 'src/assets/synapse/openmic/ATM_0353_ymzwo5.jpg',
             name: 'Open Mic'
         },
         {
             id: 2,
-            src: 'https://res.cloudinary.com/dzeuvqdbl/image/upload/v1705772154/synapse/photos%20sort%20drive/rampage/PD__9478_n1liru.jpg',
+            src: 'src/assets/synapse/rampage/PD__9452_anmn80.jpg',
             name: 'Rampage'
         },
         {
             id: 3,
-            src: 'https://res.cloudinary.com/dzeuvqdbl/image/upload/v1705772155/synapse/photos%20sort%20drive/sachin-jigar/20230318_235400_1_yefoqh.jpg',
+            src: 'src/assets/synapse/sachin-jigar/20230318_235400_1_yefoqh.jpg',
             name: 'Sachin Jigar'
         },
         {
             id: 4,
-            src: 'https://res.cloudinary.com/dzeuvqdbl/image/upload/v1705772167/synapse/photos%20sort%20drive/Samay%20Raina/DSC03081_uddqsv.jpg',
+            src: 'src/assets/synapse/SamayRaina/PXL_20230316_143155325_qxdvpu.jpg',
             name: 'Samay Raina'
         }
     ];
@@ -76,22 +76,22 @@ function Gallery() {
     const row4 = [
         {
             id: 1,
-            src: 'https://res.cloudinary.com/dzeuvqdbl/image/upload/v1705772242/synapse/photos%20sort%20drive/Vishal%20Mishra/ATM_0694_gnq1iq.jpg',
+            src: 'src/assets/synapse/Vishal Mishra/ATM_0566_qeqzxh.jpg',
             name: 'Vishal Mishra'
         },
         {
             id: 2,
-            src: 'https://res.cloudinary.com/dzeuvqdbl/image/upload/v1705773983/synapse/photos%20sort%20drive/vanmoon/DSC04951_bvz8jv.jpg',
+            src: 'src/assets/synapse/vanmoon/DSC04951_bvz8jv.jpg',
             name: 'Vanmoon'
         },
         {
             id: 3,
-            src: 'https://res.cloudinary.com/dzeuvqdbl/image/upload/v1705774160/synapse/photos%20sort%20drive/stage%20play/ATM_0106_idmjyw.jpg',
+            src: 'src/assets/synapse/stageplay/ATM_0106_idmjyw.jpg',
             name: 'Stage Play'
         },
         {
             id: 4,
-            src: 'https://res.cloudinary.com/dzeuvqdbl/image/upload/v1705774561/synapse/photos%20sort%20drive/nukkad%20nitak/PD__9982_cfguzt.jpg',
+            src: 'src/assets/synapse/nukkadnitak/PD__9982_cfguzt.jpg',
             name: 'Nukkad Natak'
         },
     ]
